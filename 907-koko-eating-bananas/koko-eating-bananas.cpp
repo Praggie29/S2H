6f02f4a9cc78ea33638cb2b6c5b610ae3ab3fdde
Pragnya Praveena Balla <164505876+Praggie29@users.noreply.github.com>
2026-08-31 16:21:28 +0530

    Time: 8 ms (54.24%) | Memory: 23 MB (58.32%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,29 +1,24 @@
 class Solution {
 public:
-    long long solve ( vector<int>& piles, int n ) {
-        long long totalHours = 0;
+    long long f ( vector<int>& piles, int h , int speed ) {
+        long long totalSpeed = 0;
         for ( int i = 0 ; i < piles.size() ; i ++ ) {
-            if ( piles[i] <= n ) totalHours += 1;
-            else {
-               totalHours += ((long long)piles[i] + n - 1) / n;
-            }
+            totalSpeed = totalSpeed + ( piles[i] + (long long)speed - 1) / speed;
         }
-        return totalHours;
+        return totalSpeed;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        
-        int low = 1 , high = *max_element(piles.begin(),piles.end());
-        int ans = high;
+        int n = piles.size();
+        int maxi = *max_element(piles.begin(),piles.end());
+        int ans;
+        int low = 1 , high = maxi;
         while ( low <= high ) {
             int mid = low + ( high - low ) / 2;
-            long long hoursNeeded = solve(piles,mid);
-            if ( hoursNeeded <= h ) {
+            if ( f(piles,h,mid) <= h ) {
                 ans = mid;
                 high = mid - 1;
             }
-            else {
-                low = mid + 1;
-            }
+            else low = mid + 1;
         }
         return ans;
     }
