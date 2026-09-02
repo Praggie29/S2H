96c9e3e1c4d09940e0ba0ac1c7f90454b0a70d22
Pragnya Praveena Balla <164505876+Praggie29@users.noreply.github.com>
2026-09-02 19:18:35 +0530

    Time: 3 ms (8.27%) | Memory: 10.4 MB (73.82%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+bool isPossible(vector<int>&nums,int k,int mid){
+    int subarrays=1;
+    int currentsum=0;
+    for(int num:nums){
+        if(currentsum+num <= mid ){
+            currentsum+=num;
+        }else{
+            subarrays++;
+            currentsum=num;
+        }
+    }
+    return subarrays <= k ;
+}
+    int splitArray(vector<int>& nums, int k) {
+       int left=*max_element(nums.begin(),nums.end());
+       int right=0;
+       for(int num:nums) right+=num;
+       while(left<right){
+        int mid=left+(right-left)/2;
+        if(isPossible(nums,k,mid)){
+            right=mid;
+        }else{
+            left=mid+1;
+        }
+       }
+       return left; 
+    }
+};
