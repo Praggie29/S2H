3476c3965863a341c33d02b79ce6f8bb168e49d4
Pragnya Praveena Balla <164505876+Praggie29@users.noreply.github.com>
2026-09-03 13:56:19 +0530

    Time: 3 ms (51.60%) | Memory: 14.2 MB (46.00%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
-    void f(int i, vector<int>& candidates, int target, int currentSum, vector<int>& store, vector<vector<int>>& ans) {
-        if (currentSum > target || i == candidates.size()) {
-            if (currentSum == target) {
-                ans.push_back(store);
-            }
+    void f ( int i ,vector<int>&candidates , vector<int>& store , vector<vector<int>>& ans , int target ) {
+        if ( i == candidates.size() || target < 0 ) return ;
+        if ( target == 0 ) {
+            ans.push_back(store);
             return;
         }
-
         store.push_back(candidates[i]);
-        f(i, candidates, target, currentSum + candidates[i], store, ans);
-
+        f ( i  , candidates , store , ans , target - candidates[i] );
         store.pop_back();
-
-        f(i + 1, candidates, target, currentSum, store, ans);
+        f ( i + 1 , candidates , store , ans , target );
     }
-
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int> store;
-        vector<vector<int>> ans;
-        f(0, candidates, target, 0, store, ans);
+        vector<int>store;
+        vector<vector<int>>ans;
+        f ( 0 , candidates , store , ans , target );
         return ans;
     }
 };
