763f46d310207f41b6b188bc4747d704a91b5c61
Pragnya Praveena Balla <164505876+Praggie29@users.noreply.github.com>
2026-09-05 10:09:11 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (11.66%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
-    int f ( int num ) {
-        if ( num < 1 ) return 0;
-        if ( num == 1 ) return 1;
-        return f ( num - 1 ) + f ( num - 2 );
+    int f ( int n , vector<int>& dp ) {
+        if ( n <= 1 ) return dp[n] = n;
+        if ( dp[n] != -1 ) return dp[n];
+        return dp[n] = f ( n - 1 , dp ) + f ( n - 2 , dp );
     }
     int fib(int n) {
-        if ( n == 0 ) return 0;
-        if ( n == 1 ) return 1;
-        return f(n-1) + f(n-2);
+        vector<int> dp ( n + 1 , -1 );
+        return f ( n , dp );
     }
 };
