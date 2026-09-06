faf2fedfb6dd1684fa3ca9d8c5fddb5cb4147a6a
Pragnya Praveena Balla <164505876+Praggie29@users.noreply.github.com>
2026-09-06 15:20:36 +0530

    Time: 23 ms (4.54%) | Memory: 82.5 MB (14.90%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
+    int f ( int i , vector<int>&nums , vector<int>&dp ) {
+        if ( i == nums.size() - 1 ) return nums[nums.size()-1];
+        if ( i >= nums.size() ) return 0;
+        if ( dp[i] != -1 ) return dp[i];
+        return dp[i] = max(nums[i],nums[i] + f ( i + 1 , nums , dp ));
+    }
     int maxSubArray(vector<int>& nums) {
         int n = nums.size();
-        int currSum = 0;
-        int maxSum = INT_MIN;
+        vector<int>dp(n,-1);
+        int maxi = INT_MIN;
         for ( int i = 0 ; i < n ; i ++ ) {
-            currSum += nums[i];
-            maxSum = max ( maxSum , currSum );
-            if ( currSum < 0 ) currSum = 0;
+            maxi = max ( maxi , f ( i , nums , dp ));
         }
-        return maxSum;
+        return maxi;
     }
 };
