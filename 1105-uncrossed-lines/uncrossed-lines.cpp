3ab40f999450b54cc53649c3a97d784d6e2cb323
Pragnya Praveena Balla <164505876+Praggie29@users.noreply.github.com>
2026-09-17 18:48:05 +0530

    Time: 11 ms (31.59%) | Memory: 16.6 MB (71.46%) - LeetSync

--- 1105-uncrossed-lines/uncrossed-lines.cpp
+++ 1105-uncrossed-lines/uncrossed-lines.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    int f ( int i , int j , int n , int m , vector<int>& nums1, vector<int>& nums2, vector<vector<int>>&dp ) {
-        if ( i == n || j == m ) return 0;
+    int f ( int i , int j , vector<int>& nums1, vector<int>& nums2 , vector<vector<int>>&dp ) {
+        int n = nums1.size();
+        int m = nums2.size();
+        if ( i >= n || j >= m ) return 0;
         if ( dp[i][j] != -1 ) return dp[i][j];
-        int a = 0;
-        if ( nums1[i] == nums2[j] ) {
-            a = 1 + f ( i + 1 , j + 1 , n , m , nums1 , nums2 , dp );
-        }
-        int b  = 0 + max ( f ( i + 1 , j , n , m , nums1 , nums2 , dp ) , f ( i , j + 1 , n , m , nums1 , nums2 , dp ));
-        return dp[i][j] = max ( a , b );
+        if ( nums1[i] == nums2[j] ) return dp[i][j] = 1 + f ( i + 1 , j + 1 , nums1 , nums2 , dp );
+        return dp[i][j] = max ( f ( i , j + 1 , nums1 , nums2 , dp ) , f ( i + 1 , j , nums1 , nums2 , dp ));
     }
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
-         int n = nums1.size();
+        int n = nums1.size();
         int m = nums2.size();
         vector<vector<int>>dp(n,vector<int>(m,-1));
-        int ans = f ( 0 , 0 , n , m , nums1 , nums2 , dp );
-        return ans == -1 ? 0 : ans;
+        return f ( 0 , 0 , nums1 , nums2 , dp );
     }
 };
